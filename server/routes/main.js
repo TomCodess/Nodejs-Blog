555f125e42bcb28ee463755cf6a72de53b01c205
Tomcodess <tom_codes@outlook.com>
2023-09-02 19:12:52 -0400

    adding new pages and more func

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,10 +6,19 @@ const router = express.Router();
 //Basic route function, app.get app.post etc. the '' is the /*something*
 // then we use a req and res with arrow function
 router.get('', (req, res) => {
-    res.send("anyong");
+    const locals = {
+        title: "Nodejs Blog",
+        description: "Simple blog page made using NodeJs, Express, and MongoDB."
+    }
+    //res.render, chooses a ejs file to render from /// Inside of {locals} can add more variables seperated by commas  
+    res.render('index', {locals});
 });
 
-
+//routes to about page
+router.get('/about', (req, res) => {
+    //res.render, chooses a ejs file to render from 
+    res.render('about');
+});
 
 
 //Exports the router so it can be used in app.js
