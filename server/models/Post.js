ec0c8df47e606f8bb9fcbb7414bbd2d8c586b504
Tomcodess <tom_codes@outlook.com>
2023-09-03 20:28:15 -0400

    finished first post schema

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 
 
+const Schema = mongoose.Schema;
 
+const PostSchema = new Schema({
+    //blog post content skeleton
+    title: {
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-const Schema = mongoose.Schema
+module.exports = mongoose.model('Post', PostSchema);
