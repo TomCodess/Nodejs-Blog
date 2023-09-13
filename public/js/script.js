46bb7f88b82e513c09b21e312923f26fda3c5786
Tomcodess <tom_codes@outlook.com>
2023-09-12 21:28:20 -0400

    typo and working fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-
     const allButtons = document.querySelectorAll('.searchBtn');
     const searchBar = document.querySelector('.searchBar');
-    const searchInput = document.getElementsById('.searchInput');
-    const searchClose = document.getElementsById('.searchClose');
+    const searchInput = document.getElementById('.searchInput');
+    const searchClose = document.getElementById('.searchClose');
 
     //adds event listeners to buttons, makes buttons usefl
     for(var i = 0; i < allButtons.length; i++){
         allButtons[i].addEventListener('click', function(){
             //manipulates the visibility of the search bar 
-            searchBar.style.visibility = 'visable';
+            searchBar.style.visibility = 'visible';
             searchBar.classList.add('open');
             this.setAttribute('aria-expanded', 'true');
             searchInput.focus();
