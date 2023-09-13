cd0a864eb3e9e4d1c9059f65cb415df21461ae41
Tomcodess <tom_codes@outlook.com>
2023-09-12 21:16:16 -0400

    more func

--- public/js/script.js
+++ public/js/script.js
@@ -6,8 +6,14 @@ document.addEventListener('DOMContentLoaded', function(){
     const searchInput = document.getElementsById('.searchInput');
     const searchClose = document.getElementsById('.searchClose');
 
+    //adds event listeners to buttons, makes buttons usefl
     for(var i = 0; i < allButtons.length; i++){
-        allButtons[i].addEventListener('click')
+        allButtons[i].addEventListener('click', function(){
+            searchBar.style.visibility = 'visable';
+            searchBar.classList.add('open');
+        }) 
+
+        
     }
 
 
