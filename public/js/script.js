3ad7b32ed99b295a0e93f41f44940e354a627edf
Tomcodess <tom_codes@outlook.com>
2023-09-12 21:23:58 -0400

    search bar bug fix

--- public/js/script.js
+++ public/js/script.js
@@ -2,20 +2,19 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
     const allButtons = document.querySelectorAll('.searchBtn');
-    const searchBar = document.querySelectorAll('.searchBar');
+    const searchBar = document.querySelector('.searchBar');
     const searchInput = document.getElementsById('.searchInput');
     const searchClose = document.getElementsById('.searchClose');
 
     //adds event listeners to buttons, makes buttons usefl
     for(var i = 0; i < allButtons.length; i++){
         allButtons[i].addEventListener('click', function(){
+            //manipulates the visibility of the search bar 
             searchBar.style.visibility = 'visable';
             searchBar.classList.add('open');
+            this.setAttribute('aria-expanded', 'true');
+            searchInput.focus();
         }) 
-
-        
     }
-
-
 });
 
