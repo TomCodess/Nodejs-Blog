802dc4fae3e63594bd79c7317c79b42fec3b29c3
Tomcodess <tom_codes@outlook.com>
2023-09-24 17:35:17 -0400

    Project reimage

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
 require('dotenv').config();
-//something is broke
+
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
-//const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
 const app = express();
-//or statment is for when if I want to upload to server and not host locally
-const PORT = 5000 || process.env.PORT;
-
-//connect to database
+const PORT = process.env.PORT || 5000;
+  
+// Connect to DB
 connectDB();
 
-//Middleware to be able to search for posts on the page
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
+app.use(methodOverride('_method'));
 
 app.use(session({
-    secret: 'idk4fun',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
-    }),
-    //If i want to set a max cookie age
-    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
 }));
 
-//make it use public folder for styling 
 app.use(express.static('public'));
 
-//Templating Engine
+// Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-//Routes the endpoint to server folder so avoid clutter
+
+app.locals.isActiveRoute = isActiveRoute; 
+
+
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
+app.listen(PORT, ()=> {
+  console.log(`App listening on port ${PORT}`);
 });
